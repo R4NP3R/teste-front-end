647a8f4b79898800e347972f1b222ef546047006
R4NP3R <ssrafa.2014@gmail.com>
2025-03-13 22:01:01 -0300

    fix: arrumando a imagem principal do site

--- teste-front-end/src/components/main/main.tsx
+++ teste-front-end/src/components/main/main.tsx
@@ -10,7 +10,13 @@ export const Main = () => {
   
   return (
     <main>
-      <img className='principal-image' src={blackFridayImg} alt="loja de roupas com black friday" />
+      <div className="principal-image">
+        <img className="principal-image-background" src={blackFridayImg} alt="imagem de uma loja de roupas em black friday" />
+        <h2 className="principal-image-title">Venha conhecer nossas promoçoes</h2>
+        <p className="principal-image-description"><span>50% Off</span> nos produtos</p>
+        <button className="principal-image-button">Ver Produto</button>
+      </div>
+      {/* <img className='principal-image' src={blackFridayImg} alt="loja de roupas com black friday" /> */}
       <ShopSectionList/>
       <ProductsSection withPartners productFilter/>
       <ProductsSection withPartners productFilter={false}/>
